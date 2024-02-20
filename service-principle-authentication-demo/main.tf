d1e0a8220aff0b903a390d9d12625eef5e1b940d
Vijay Vishwakarma <vijay.vishwakarma@citiustech.com>
2024-02-20 16:51:31 +0530

    Files Changes

--- service-principle-authentication-demo/main.tf
+++ service-principle-authentication-demo/main.tf
@@ -9,10 +9,10 @@ terraform {
 
 provider "azurerm" {
   # Configuration options
-  subscription_id = "d257be94-af42-44f3-8210-e0dddd2babce"
-  tenant_id = "e35c7dbb-8668-4871-9198-6e577cc4af23"
-  client_secret = "fru8Q~ChEbpA0-sWVToayeLQSAdY7skLrDuQOdwo"
-  client_id = "e3629765-d40e-44b7-9510-be1b7a718efb"
+  subscription_id = ""
+  tenant_id = ""
+  client_secret = ""
+  client_id = ""
 
   features {
     
