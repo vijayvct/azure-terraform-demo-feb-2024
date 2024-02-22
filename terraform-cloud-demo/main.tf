680cc42ad7b422a9a9ec7442d67cb6907cfa87e2
Vijay Vishwakarma <vijay.vishwakarma@citiustech.com>
2024-02-22 15:15:24 +0530

    Files Modified

--- terraform-cloud-demo/main.tf
+++ terraform-cloud-demo/main.tf
@@ -19,10 +19,10 @@ provider "azurerm" {
   features {
     
   }
-  subscription_id = "d257be94-af42-44f3-8210-e0dddd2babce"
-  tenant_id = "e35c7dbb-8668-4871-9198-6e577cc4af23"
-  client_secret = "FAg8Q~Lyd32kBZR51yY08E_Ow0oXW1hla.P_eddn"
-  client_id = "922f8eaa-1124-46ed-95f0-a57b40c01a3a"
+  subscription_id = ""
+  tenant_id = ""
+  client_secret = ""
+  client_id = ""
 }
 
 resource "azurerm_resource_group" "myrg" {
